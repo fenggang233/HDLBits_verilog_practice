42bcd371f3c3cac2d89b1cc0b39bdc27b386117b
fenggang233 <55091802+fenggang233@users.noreply.github.com>
2022-08-14 13:17:56 +0800

    Update Kmap4.v

--- circuits/Kmap4.v
+++ circuits/Kmap4.v
@@ -4,7 +4,8 @@ module top_module(
     input c,
     input d,
     output out  ); 
-    assign out = ((a^b)&~(c^d)) | ((c^d)&~(a^b));
+    //assign out = ((a^b)&~(c^d)) | ((c^d)&~(a^b));
+    assign out = (a^b)^(c^d);
 endmodule
 
 //没用卡诺简化，但是这样更简单
